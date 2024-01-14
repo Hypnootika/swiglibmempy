0006aa4d697a2996444825d84144387a34210ff3
Hypnotika <phillip troeller@outlook.com>
2024-01-14 23:23:52 +0100

    update

--- cmake/cpp.cmake
+++ cmake/cpp.cmake
@@ -1,5 +1,11 @@
+enable_language(CXX)
+set(CMAKE_CXX_STANDARD 17)
+set(CMAKE_CXX_STANDARD_REQUIRED ON)
+set(CMAKE_CXX_EXTENSIONS OFF)
+
 add_subdirectory(Libmem)
 include(GNUInstallDirs)
+
 install(EXPORT ${PROJECT_NAME}Targets
         NAMESPACE ${PROJECT_NAMESPACE}::
         DESTINATION ${CMAKE_INSTALL_LIBDIR}/cmake/${PROJECT_NAME}
--- cmake/python.cmake
+++ cmake/python.cmake
@@ -28,6 +28,30 @@ function(search_python_module)
         endif()
     endif()
 endfunction()
+function(search_python_internal_module)
+  set(options "")
+  set(oneValueArgs NAME)
+  set(multiValueArgs "")
+  cmake_parse_arguments(MODULE
+    "${options}"
+    "${oneValueArgs}"
+    "${multiValueArgs}"
+    ${ARGN}
+  )
+  message(STATUS "Searching python module: \"${MODULE_NAME}\"")
+  execute_process(
+    COMMAND ${Python3_EXECUTABLE} -c "import ${MODULE_NAME}"
+    RESULT_VARIABLE _RESULT
+    ERROR_QUIET
+    OUTPUT_STRIP_TRAILING_WHITESPACE
+    )
+  if(${_RESULT} STREQUAL "0")
+    message(STATUS "Found python internal module: \"${MODULE_NAME}\"")
+  else()
+    message(FATAL_ERROR "Can't find python internal module \"${MODULE_NAME}\", please install it using your system package manager.")
+  endif()
+endfunction()
+
 
 list(APPEND CMAKE_SWIG_FLAGS "-I${PROJECT_SOURCE_DIR}")
 set(PYTHON_PROJECT "py${PROJECT_NAME}")
@@ -51,20 +75,23 @@ add_custom_command(
         COMMAND ${CMAKE_COMMAND} -E make_directory ${PYTHON_PROJECT}/.libs
         # Don't need to copy static lib on Windows.
         COMMAND ${CMAKE_COMMAND} -E $<IF:$<STREQUAL:$<TARGET_PROPERTY:Libmem,TYPE>,SHARED_LIBRARY>,copy,true>
+        $<$<STREQUAL:$<TARGET_PROPERTY:Libmem,TYPE>,SHARED_LIBRARY>:$<TARGET_SONAME_FILE:Libmem>>
+        ${PYTHON_PROJECT}/.libs
+        COMMAND ${CMAKE_COMMAND} -E $<IF:$<STREQUAL:$<TARGET_PROPERTY:Libmem,TYPE>,SHARED_LIBRARY>,copy,true>
         COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:pyLibmem> ${PYTHON_PROJECT}/libmem
         COMMAND ${Python_EXECUTABLE} setup.py bdist_wheel
         COMMAND ${CMAKE_COMMAND} -E touch ${PROJECT_BINARY_DIR}/python/dist/timestamp
         MAIN_DEPENDENCY
-        python/setup.py.in
+            python/setup.py.in
         DEPENDS
-        python/setup.py
+            python/setup.py
         ${PROJECT_NAMESPACE}::Libmem
         ${PROJECT_NAMESPACE}::pyLibmem
         BYPRODUCTS
-        python/${PYTHON_PROJECT}
-        python/${PYTHON_PROJECT}.egg-info
-        python/build
-        python/dist
+            python/${PYTHON_PROJECT}
+            python/${PYTHON_PROJECT}.egg-info
+            python/build
+            python/dist
         WORKING_DIRECTORY python
         COMMAND_EXPAND_LISTS)
 
